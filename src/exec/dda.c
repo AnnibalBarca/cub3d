b42f99bb6774884d1454bbf2b157d586bf0f38dc
Alexandre Meekel-Konjikovic <almeekel@z3r11p1.42lyon.fr>
2025-10-27 19:32:18 +0100

    pos

--- src/exec/dda.c
+++ src/exec/dda.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dda.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nagaudey <nagaudey@student.42.fr>          +#+  +:+       +#+        */
+/*   By: almeekel <almeekel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 14:16:34 by almeekel          #+#    #+#             */
-/*   Updated: 2025/10/27 17:51:58 by nagaudey         ###   ########.fr       */
+/*   Updated: 2025/10/27 19:32:02 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,23 +36,23 @@ void	dda_side_dist_delta_init(t_game *game, t_ray *r)
 	}
 }
 
-void	dda_init_ray(t_game *game, t_ray *r, int screen_x)
-{
-	double	camera_x;
+// void	dda_init_ray(t_game *game, t_ray *r, int screen_x)
+// {
+// 	double	camera_x;
 
-	camera_x = 2.0 * screen_x / (double)WIN_WIDTH - 1.0;
-	r->camera_x = camera_x;
-	r->raydir_x = game->player.dir_x + game->player.plane_x * camera_x;
-	r->raydir_y = game->player.dir_y + game->player.plane_y * camera_x;
-	r->map_x = (int)game->player.pos_x;
-	r->map_y = (int)game->player.pos_y;
-	dda_iter_limit(game, r);
-	r->delta_distx = fabs(1.0 / r->raydir_x);
-	r->delta_disty = fabs(1.0 / r->raydir_y);
-	dda_side_dist_delta_init(game, r);
-	r->hit = 0;
-	r->side = 0;
-}
+// 	camera_x = 2.0 * screen_x / (double)WIN_WIDTH - 1.0;
+// 	r->camera_x = camera_x;
+// 	r->raydir_x = game->player.dir_x + game->player.plane_x * camera_x;
+// 	r->raydir_y = game->player.dir_y + game->player.plane_y * camera_x;
+// 	r->map_x = (int)game->player.pos_x;
+// 	r->map_y = (int)game->player.pos_y;
+// 	dda_iter_limit(game, r);
+// 	r->delta_distx = fabs(1.0 / r->raydir_x);
+// 	r->delta_disty = fabs(1.0 / r->raydir_y);
+// 	dda_side_dist_delta_init(game, r);
+// 	r->hit = 0;
+// 	r->side = 0;
+// }
 // appeller avec la colonne horizontale de pixel screen-x
 int	ft_dda(t_game *game, t_ray *r, int screen_x)
 {
